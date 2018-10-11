d4ea3a39a2dd5ed0372171caca0351aa6b99e240
Faraz Haider <frz.hdr.619@gmail.com>
2018-10-11 17:29:15 +0530

    Changes for hex.

--- mix.exs
+++ mix.exs
@@ -4,17 +4,37 @@ defmodule CSMT.MixProject do
   def project do
     [
       app: :csmt,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp description do
+    "Compact Sparse Merkle Trees (CSMT) with pluggable storage."
+  end
+
+  defp package do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "csmt",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE),
+      maintainers: ["Faraz Haider"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/ZanjeerPlatform/csmt"
+      }
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:exprotobuf, :logger]
     ]
   end
 
